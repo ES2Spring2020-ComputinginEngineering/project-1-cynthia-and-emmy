9222d2992044c631e1ff88809308feadf42c0dcd
cynthiajelke <60204619+cynthiajelke@users.noreply.github.com>
2020-03-04 23:24:22 -0500

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,51 @@
+##################
+# Emmy Daro and Cynthia Jelke
+# This Program collects pendulum measurements of acceleration and time and
+# sends them to a reciever
+# Time to Complete:
+#################
+
+import microbit as mb
+import radio  # Needs to be imported separately
+
+# Change the channel if other microbits are interfering. (Default=7)
+radio.on()  # Turn on radio
+radio.config(channel=7, length=100)
+
+print('Program Started')
+mb.display.show(mb.Image.HAPPY)
+
+while not mb.button_a.is_pressed():  # wait for button A to be pressed to begin logging
+    mb.sleep(10)
+
+radio.send('start') # Send the word 'start' to start the receiver
+mb.sleep(1000)
+mb.display.show(mb.Image.HEART)  # Display Heart while logging
+
+
+# Read and send accelerometer data repeatedly until button A is pressed again
+while not mb.button_a.is_pressed():
+    ######################################################
+    # FILL In HERE
+    # Need to collect accelerometer and time measurements
+    # Need to format into a single string
+    # Send the string over the radio
+    x_ac=mb.accelerometer.get_x()
+
+    y_ac=mb.accelerometer.get_y()
+
+    z_ac=mb.accelerometer.get_z()
+
+    time=mb.running_time()
+
+    #puts together a string for the logger to send to the receiver
+    message= str(time) + ',' + str(x_ac) + ',' + str(y_ac) + ',' + str(z_ac)
+    ######################################################
+
+    #sends the info
+    radio.send(message)
+    mb.sleep(10)
+
+
+
+mb.display.show(mb.Image.SQUARE)  # Display Square when program ends
--- receiver.py
+++ receiver.py
@@ -1,42 +1,82 @@
 
+
 ##################
+
 # Emmy Daro and Cynthia Jelke
+
 # This Program converts the data received in logger.py into data that can be
+
 # plotted and visualized
+
 # Time to Complete:
+
 #################
 
+
+
 import microbit as mb
+
 import radio  # Needs to be imported separately
 
+
+
 # Change the channel if other microbits are interfering. (Default=7)
+
 radio.on()  # Turn on radio
+
 radio.config(channel=7, length =100)
 
+
+
 print('Program Started')
+
 mb.display.show(mb.Image.HAPPY, delay=1000, clear=True)
 
 
+
+
+
 # Wait for start message before beginning printing
+
 incoming = ''
+
 while not incoming == 'start':
+
     incoming = radio.receive()
+
 print('start')
 
 
+
+
+
 while True:
+
     incoming = radio.receive() # Read from radio
 
+    mb.sleep(5)
+    #print("This is incoming")
+    #print(incoming)
+
     if incoming is not None: # message was received
+
         mb.display.show(mb.Image.HEART, delay=100, clear=True, wait=False)
 
-        #############################################################
-        # FILL IN HERE
-        # Incoming is string sent from logger
-        # Need to parse it and reformat as a tuple for the MU plotter
-        #############################################################
-        for data in incoming.split(','):
-            values=tuple(int(data))
-        print(values)
 
-        mb.sleep(10)
+
+        #############################################################
+
+        # FILL IN HERE
+
+        # Incoming is string sent from logger
+
+        # Need to parse it and reformat as a tuple for the MU plotter
+
+        #############################################################
+
+
+        value = eval(incoming)#
+
+        #makes it so that you can see the values on the REPL and Plotter
+        print(value)
+        mb.sleep(100)
